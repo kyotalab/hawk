c076d58fb90d9f59f29ceee0da7f03903d06223f
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-07-09 06:19:34 +0900

    Implement field access logic using if-else

--- src/error.rs
+++ src/error.rs
@@ -2,10 +2,18 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("File not found: {0}")]
+    FileNotFound(#[from] std::io::Error), // std::io::Error から自動変換
+
     #[error("JSON deserialization error: {0}")]
     Json(#[from] serde_json::Error), // serde_json::Error から自動変換
     
     #[error("str parse int error: {0}")]
     StrToInt(#[from] std::num::ParseIntError), // std::num::ParseIntError からの自動変換
 
+    #[error("Invalid query format: {0}")]
+    InvalidQuery(String),
+
+    #[error("Array index out of bounds: {0}")]
+    IndexOufOfBounds(usize),
 }
--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,35 @@ use anyhow::Result;
 use hawk::Error;
 use serde_json::Value;
 
-fn main() -> Result<()>{
+fn main() -> Result<(), Error>{
     let file = File::open("users.json")?;
     let reader = BufReader::new(file);
     let json: Value = serde_json::from_reader(reader)?;
 
     // `.users[0].name`
-    let query = ".users[abc].name";
-    let segments = query.split('.').collect::<Vec<&str>>();
-    let segment = segments[1]; 
-    let param = segments[2];
+    let query = ".users.name";
+    let mut segments = query.split('.');
+    segments.next();
+    let segment = segments.next().unwrap(); 
+    let param = segments.next().unwrap();
 
     // debug
-    println!("{}", segment);
-    println!("{}", param);
+    // println!("{}", segment);
+    // println!("{}", param);
 
-    let idx = segment.find('[').unwrap();
-    let ridx = segment.find(']').unwrap();
 
-    // debug
-    println!("{}", idx);
-    println!("{}", ridx);
 
     if segment.contains('[') && segment.contains(']') {
+        let idx = segment.find('[').unwrap();
+        let ridx = segment.find(']').unwrap();
+
+        if idx >= ridx {
+            return Err(Error::InvalidQuery("Invalid bracket order".into()));
+        }
+        // debug
+        // println!("{}", idx);
+        // println!("{}", ridx);
+
         let json_key = segment.get(..idx).unwrap();
         let index = segment.get(idx + 1..ridx).unwrap().parse::<usize>().map_err(|e| {
             Error::StrToInt(e)
@@ -34,15 +40,43 @@ fn main() -> Result<()>{
 
 
         // debug
-        println!("{:?}", json_key);
-        println!("{:?}", index);
+        // println!("{:?}", json_key);
+        // println!("{:?}", index);
 
-        let values = json.get(json_key).unwrap();
+        let values = json.get(json_key).ok_or(Error::InvalidQuery(format!("Key '{}' not found", json_key)))?;
         let res = &values[index].get(param);
 
         // output
         println!("{:?}", res);
 
+    } else {
+        let json_key = segment;
+
+        let values = json.get(json_key).ok_or(Error::InvalidQuery(format!("Key '{}' not found", json_key)))?;
+        let values_arr = values.as_array().unwrap();
+
+        // debug
+        // println!("{:?}", values_arr);
+
+        let mut res = Vec::new();
+
+        for value in values_arr.iter() {
+            res.push(value.get(param).unwrap());
+        }
+
+        let res: Vec<_> = values_arr.iter()
+            .map(|value| value.get(param).unwrap())
+            .collect();
+        
+        let res: Vec<String> = res.into_iter()
+            .map(|v| match v {
+                Value::String(s) => s.clone(),
+                _ => v.to_string().trim_matches('"').to_string()
+            })
+            .collect();
+
+        // output
+        println!("{:?}", res);
     }
 
     Ok(())
