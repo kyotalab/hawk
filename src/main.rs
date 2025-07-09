5a9cdc29a6f0cebed1fa3e3816f098f4c6ac00a5
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-07-09 13:58:04 +0900

    Refactor: function modularization

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use std::{fs::File, io::{self, BufRead, BufReader}};
-
 use anyhow::Result;
-use clap::Parser;
-use hawk::{execute_query, Args, Error};
-use serde_json::Value;
+use hawk::{execute_query, setup, Error};
 
 fn main() -> Result<(), Error>{
     // Load CLI arguments and file
@@ -16,20 +12,6 @@ fn main() -> Result<(), Error>{
     Ok(())
 }
 
-
-pub fn setup() -> Result<(Value, String), Error> {
-    let args = Args::parse();
-    let reader: Box<dyn BufRead> = if let Some(path) = args.path {
-        Box::new(BufReader::new(File::open(path)?))
-    } else {
-        Box::new(BufReader::new(io::stdin()))
-    };
-
-    let json: Value = serde_json::from_reader(reader)?;
-    let query = args.query;
-
-    Ok((json, query))
-}
 
 
 
